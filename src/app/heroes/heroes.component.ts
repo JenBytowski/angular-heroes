ee33ec34ac6ef0f0d9e5e8fc6deb380ca6e55437
Yauheni Tabakerau <Yauheni.Tabakerau@itechart-group.com>
2021-03-04 10:40:17 +0300

    refactor heroes-component and add some styles

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,18 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = { id: 1, name: 'Windstorm', age: 34};
+  heroes: Hero[] = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
+
+  onSelect(hero: Hero) : void
+  {
+    this.selectedHero = hero;
+  }
 
   ngOnInit(): void {
   }
 
 }
 
-
 export interface Hero {
   id: number;
   name: string;
   age?: number;
 }
+
+export const HEROES: Hero[] = [
+{id: 1, name: 'Windstorm', age:10},
+{id: 2, name: 'Mr Someone', age:100},
+{id: 3, name: 'Tornado'},
+{id: 4, name: 'Lizzard king', age:27},
+];
